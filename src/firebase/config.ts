a3d736fbbb5b4b11e77adb2e203f82650c3e4822
Moses Muthii <mosesmuthii43@gmail.com>
2026-03-25 23:02:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3831689628-1b5cc",
+  "appId": "1:623823417590:web:3aeb1d8cd71fd4d09c3cd7",
+  "apiKey": "AIzaSyB2FQP9IgbhPUJU_i1aSbr0ai-u0nFxW6s",
+  "authDomain": "studio-3831689628-1b5cc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "623823417590"
+};

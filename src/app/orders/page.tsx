de61cfdce4eefb36ff137121ddc2449b5d3ede7c
Moses Muthii <mosesmuthii43@gmail.com>
2026-03-29 18:34:41 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -29,7 +29,7 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogClose,
-} from "@/dialog";
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from '@/lib/utils';
